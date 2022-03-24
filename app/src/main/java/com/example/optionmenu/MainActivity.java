1f4ac52c40852479ea504a30a1c8b0b3f140e8d9
pkumaresangithub <pkumaresanin@gmail.com>
2022-03-24 20:33:09 +0530

    first commit

---
+++ app/src/main/java/com/example/optionmenu/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.optionmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.optionmenu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.call:
+                Toast.makeText(getApplicationContext(),"Call ",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.sms:
+                Toast.makeText(getApplicationContext(),"SMS",Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
