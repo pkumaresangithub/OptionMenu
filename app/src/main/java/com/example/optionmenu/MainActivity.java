e9cbeaf27521ddddd5743fbcbb44f4b3f7b38da4
pkumaresangithub <pkumaresanin@gmail.com>
2022-03-28 20:53:06 +0530

    first commit

--- app/src/main/java/com/example/optionmenu/MainActivity.java
+++ app/src/main/java/com/example/optionmenu/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.optionmenu;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,9 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
+        Intent intent = new Intent(MainActivity.this,Scrollview.class);
+        startActivity(intent);
 
     }
 
